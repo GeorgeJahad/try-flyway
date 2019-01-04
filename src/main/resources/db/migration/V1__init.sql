791125599aaaa671df3b7a458ada18b63accd090
George Jahad <github@blackbirdsystems.net>
2019-01-03 17:24:58 -0800

    corrected migration script

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -9,11 +9,10 @@ CREATE TABLE resources (
 INSERT INTO resources(identifier_name, identifier_value, tenant_id) VALUES('hostname', 'MV906GG8WL', 'aaaaaa');
 
 CREATE TABLE labels (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
+  id bigint(20) NOT NULL,
   labels varchar(100) NOT NULL,
   labels_key varchar(100) NOT NULL,
-  PRIMARY KEY (id),
   );
-INSERT INTO labels(labels_key, labels) VALUES('os', 'DARWIN');
-INSERT INTO labels(labels_key, labels) VALUES('hostname', 'MV906GG8WL');
-INSERT INTO labels(labels_key, labels) VALUES('arch', 'X86_64');
+INSERT INTO labels(labels_key, labels, id) VALUES('os', 'DARWIN', 1);
+INSERT INTO labels(labels_key, labels, id) VALUES('hostname', 'MV906GG8WL', 1);
+INSERT INTO labels(labels_key, labels, id) VALUES('arch', 'X86_64', 1);
